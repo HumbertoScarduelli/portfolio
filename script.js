2f6506b83a839e5ef0a5c8eb35c12e46356700f4
HumbertoScarduelli <humbertoscarduelli@gmail.com>
2026-03-23 19:28:05 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+document.getElementById("form").addEventListener("submit", function(e) {
+  e.preventDefault();
+
+  let nome = document.getElementById("nome").value;
+  let email = document.getElementById("email").value;
+  let mensagem = document.getElementById("mensagem").value;
+
+  if(nome === "" || email === "" || mensagem === "") {
+    alert("Preencha todos os campos!");
+    return;
+  }
+
+  if(!email.includes("@")) {
+    alert("Email inválido!");
+    return;
+  }
+
+  function toggleDarkMode() {
+  document.body.classList.toggle("dark");
+}
+
+  alert("Mensagem enviada com sucesso!");
+  document.getElementById("form").reset();
+});
